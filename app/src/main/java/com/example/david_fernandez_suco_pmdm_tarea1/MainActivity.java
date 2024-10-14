32da0c2a7062549f995c54615f7e6aa60a295eb2
David Suco <dfsuco@gmail.com>
2024-10-14 11:40:45 +0200

    avanzar hasta actividad 2

--- app/src/main/java/com/example/david_fernandez_suco_pmdm_tarea1/MainActivity.java
+++ app/src/main/java/com/example/david_fernandez_suco_pmdm_tarea1/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.david_fernandez_suco_pmdm_tarea1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //obtener los botones a través de sus ids
+        Button buttonIniciarSesion = findViewById(R.id.buttonIniciarSesion);
+        Button buttonCredenciales = findViewById(R.id.buttonCredenciales);
+
+
+        //obtener los editText a través de sus ids
+        EditText editTextNombre = findViewById(R.id.editTextNombre);
+        EditText editTextContraseña = findViewById(R.id.editTextContraseña);
+
+        //
+
+        //añadir un listener al boton iniciar sesion
+        buttonIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //comprobar que el texto introducido en los dos editTexts corresponden a admin
+                if (editTextNombre.getText().toString().equals("admin")
+                        && editTextContraseña.getText().toString().equals("admin")) {
+                    //si se cumple la condicion ir a la siguiente actividad a través de un intent explicito
+                    Intent intentExplicito = new Intent(MainActivity.this, Actividad2.class);
+                } else {
+                    //si no se cumple mostrar el mensaje correspondiente
+                    Toast.makeText(MainActivity.this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
+
+
+
+
